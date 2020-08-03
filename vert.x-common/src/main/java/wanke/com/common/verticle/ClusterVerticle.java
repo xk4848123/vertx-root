3bb149929b2c7e50aaed602c14e63f7264d54410
chendi <15395068264@163.com>
2020-08-03 17:37:39 +0800

    commit for all

--- vert.x-common/src/main/java/wanke/com/common/verticle/ClusterVerticle.java
+++ vert.x-common/src/main/java/wanke/com/common/verticle/ClusterVerticle.java
@@ -1,22 +1,15 @@
 package wanke.com.common.verticle;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.eventbus.EventBus;
-import io.vertx.core.http.HttpServer;
-import io.vertx.ext.web.Router;
 import wanke.com.common.annotion.Controller;
 import wanke.com.common.annotion.RequestMapping;
 import wanke.com.common.config.VertxConfig;
-import wanke.com.common.flat.FlatBuffersCommonMsg;
 import wanke.com.common.log.LogUtil;
 import wanke.com.common.messagecodec.FlatBuffersMessageCodec;
 import wanke.com.common.messagecodec.ProtoMessageCodec;
 import wanke.com.common.msghandle.WrapMsg;
 import wanke.com.common.util.ScanPackageClassUtil;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
 import java.util.List;
@@ -31,7 +24,6 @@ public class ClusterVerticle  extends AbstractVerticle {
         EventBus eventBus = vertx.eventBus();
         eventBus.registerCodec(protoMessageCodec);
         eventBus.registerCodec(flatBuffersMessageCodec);
-
 
         //扫描
         List<String> controllerList = null;
@@ -53,19 +45,31 @@ public class ClusterVerticle  extends AbstractVerticle {
                         continue;
                     }
                     Type t = method.getAnnotatedReturnType().getType();
-                    if (!t.getTypeName().equals("java.util.Map") && !t.getTypeName().equals("java.util.HashMap")){
-                        LogUtil.errorDirect("must method with Map or HashMap");
+                    if (!t.getTypeName().equals("java.util.Map") && !t.getTypeName().equals("java.util.HashMap") && !t.getTypeName().equals("void")){
+                        LogUtil.errorDirect("must method with Map or HashMap or void");
                         System.exit(-1);
                     }
                     String secondRMString = curMethodRM.value();
                     eventBus.consumer(firstRMString + "." + secondRMString, msg -> {
-                        WrapMsg wrapMsg = new WrapMsg();
-                        wrapMsg.setInnerMsg(msg);
-                        try {
-                            Object reply = method.invoke(o, wrapMsg);
-                            wrapMsg.reply((Map) reply);
-                        } catch (Exception e) {
-                            LogUtil.error(e.getMessage());
+                        WrapMsg wrapMsgReq = new WrapMsg();
+                        wrapMsgReq.setInnerMsg(msg);
+                        if (t.getTypeName().equals("void")){
+                            try {
+                                method.invoke(o, wrapMsgReq);
+                            } catch (Exception e) {
+                                LogUtil.error(e.getMessage());
+                            }
+                        }else {
+                            vertx.executeBlocking(future -> {
+                                try {
+                                    Object reply = method.invoke(o, wrapMsgReq);
+                                    future.complete(reply);
+                                } catch (Exception e) {
+                                    LogUtil.error(e.getMessage());
+                                }
+                            }, res -> {
+                                wrapMsgReq.reply((Map) res.result());
+                            });
                         }
                     });
                 }
@@ -74,8 +78,5 @@ public class ClusterVerticle  extends AbstractVerticle {
             LogUtil.errorDirect(e.getMessage());
             System.exit(-1);
         }
-
-
-
     }
 }
